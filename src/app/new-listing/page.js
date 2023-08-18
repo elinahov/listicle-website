47c739474862280d1665d0ab99b0f0062bd84852
Elina Hovakimyan <elina.hovakimyan@firstvet.com>
2023-08-18 15:46:12 +0400

    Add form inputs

--- src/app/new-listing/page.js
+++ src/app/new-listing/page.js
@@ -3,6 +3,8 @@ import Header from "@/components/Header";
 import styles from './page.module.scss';
 import { useState } from "react";
 import Image from "next/image";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
 
 const AddNewListing = () => {
     const [selectedImage, setSelectedImage] = useState()
@@ -16,24 +18,35 @@ const AddNewListing = () => {
 
             <h2 className={styles.title}>Add New Listing</h2>
 
-            <p className={styles.label}>Upload File</p>
+            <div className={styles.container}>
+                <div className={styles.uploadContainer}>
+                    <p className={styles.label}>Upload File</p>
 
-
-            {selectedImage ? (
-                <div className={styles.uploadedImageContainer}>
-                    <Image className={styles.uploadedImage} alt="uplaoded image" src={URL.createObjectURL(selectedImage)} width={250} height={250} />
-                    <Image onClick={() => setSelectedImage(null)} className={styles.removeImage} alt="delete" src={'/close.png'} width={32} height={32} />
-                </div>
-            ) : (
-                <div className={styles.uploadWrapper}>
-                    <div className={styles.customUpload}>
-                        <div className={styles.plusContainer}>
-                            <h4>+</h4>
+                    {selectedImage ? (
+                        <div className={styles.uploadedImageContainer}>
+                            <Image className={styles.uploadedImage} alt="uplaoded image" src={URL.createObjectURL(selectedImage)} width={250} height={250} />
+                            <Image onClick={() => setSelectedImage(null)} className={styles.removeImage} alt="delete" src={'/close.png'} width={32} height={32} />
                         </div>
-                    </div>
-                    <input className={styles.uploadInput} type="file" name="image" onChange={onImageSelect} />
+                    ) : (
+                        <div className={styles.uploadWrapper}>
+                            <div className={styles.customUpload}>
+                                <div className={styles.plusContainer}>
+                                    <h4>+</h4>
+                                </div>
+                            </div>
+                            <input className={styles.uploadInput} type="file" name="image" onChange={onImageSelect} />
+                        </div>
+                    )}
                 </div>
-            )}
+
+                <div className={styles.form}>
+                    <Input label="Title" />
+                    <Input label="Price" />
+
+                    <Button className={styles.submitButton}>Submit</Button>
+                </div>
+
+            </div>
         </div>
     )
 }
